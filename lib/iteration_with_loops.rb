ad403d9c8882831c919d1a19d52dfa20764e9271
Liz Burke <liz.burke.chi@gmail.com>
2021-01-16 01:02:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     binding.pry
 while row_index < src.count do
     element_index = 0
-    binding.pry
+
   while element_index < src[row_index].count do
     if src[row_index][element_index] == String
 

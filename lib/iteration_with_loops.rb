ccb52b5f319de06727c4e1e38e903ccd0b0d40d3
Liz Burke <liz.burke.chi@gmail.com>
2021-01-16 00:43:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row_index < src.count do
     element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index] == String
-      join_nested_strings << src[row_index][element_index]
+      join_nested_strings.join src[row_index][element_index]
     end
     element_index += 1
 end
